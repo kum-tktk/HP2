303960719f4a44e1aa134946bb4a69a2450f28aa
kumiko.takado <kum.t9393@gmail.com>
2021-07-09 13:20:01 +0900

    HP

---
+++ mail.php
@@ -0,0 +1,143 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$site_top = "index.html";
+
+
+%$to = "メールアドレス";
+
+
+
+$from = "メールアドレス";
+
+
+$Email = "メールアドレス"
+
+
+
+
+
+
+
+$Referer_check = 1;
+
+
+
+$Referer_check_domain = "ドメイン(withcode.tech)";
+
+
+
+
+$useToken = 1;
+
+
+
+
+
+
+
+
+
+$userMail = 1;
+
+
+$BccMail = "";
+
+
+$subject = "ホームページのお問い合わせ"；
+
+
+$confirmDsp = 1;
+
+
+
+
+$jumpPage = 0;
+
+
+
+
+
+$requireCheck = 1;
+
+
+
+
+$require = array('お問い合わせ項目'，'お名前(漢字)','メールアドレス','お問い合わせ内容´);
+
+
+
+
+
+
+
+
+$remail = 1;
+
+
+$refrom_name = "WithCode";
+
+
+$re_subject = "お問い合わせ頂き誠に有難う御座います"
+
+
+
+$dsp_name = 'お名前(漢字)';
+
+
+$remail_text = <<< TEXT
+
+お問い合わせありがとうございました。
+確認し次第、早急にご返信致しますので今しばらくお待ち下さい。
+
+送信内容は以下になります。
+
+TEXT;
+
+
+
+$mailFooterDsp = 1;
+
+
+$mailSignature = <<< FOOTER
+
+──────────────────────
+株式会社○○○○　WithCode
+〒150-XXXX 東京都○○区○○ 　○○ビル○F　
+TEL：03- XXXX - XXXX 　FAX：03- XXXX - XXXX
+携帯：090- XXXX - XXXX 　
+E-mail:xxxx@xxxx.com
+──────────────────────
+
+FOOTER;
+
+
